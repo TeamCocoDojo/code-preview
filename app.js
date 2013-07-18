facc2f59666c3cc8798a2321f56a5d9b8415489a
clydeli <clyde1008li@gmail.com>
2013-07-18 13:11:14 -0700

    add a simple clone function...

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-var io = require('socket.io').listen(8888);
+var
+  io = require('socket.io').listen(3333),
+  sys = require('sys'),
+  exec = require('child_process').exec;
+
+function puts(error, stdout, stderr) { sys.puts(stdout) }
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
 
-  socket.on('clone', function (data) {
+  socket.emit('livePreview', {});
+
+  socket.on('clone', function(data){
     console.log(data);
+    exec("git clone "+data.repoAddr+" sessions/"+data.sessionId, puts);
   });
+
 });

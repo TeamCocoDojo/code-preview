69430cc5cb2a34c8dce111abe141e7431fb6d61c
clydeli <clyde1008li@gmail.com>
2013-07-18 14:17:56 -0700

    basic version of socket communication

---
+++ app-client.js
@@ -0,0 +1,8 @@
+var liveSocket = io.connect("http://ec2-54-245-222-60.us-west-2.compute.amazonaws.com", {port: 3333} );
+liveSocket.on('livePreview', function(data){
+  console.log(data);
+  // example one...
+  liveSocket.emit("clone", {sessionId: "testid", repoAddr: "git@github.com:davliu/sticky.git"} );
+  // example two...
+  liveSocket.emit("replaceFile", {sessionId: "testid", fileString: "teststring", path: 'index.html'} );
+});
--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 var
-  io = require('socket.io').listen(3333),
+  app = require('express'),
+  http = require('http'),
+  server = http.createServer(app)
+  io = require('socket.io').listen(server),
+  fs = require('fs'),
   sys = require('sys'),
   exec = require('child_process').exec;
+
+server.listen(3333);
 
 function puts(error, stdout, stderr) { sys.puts(stdout) }
 
@@ -14,4 +20,15 @@ io.sockets.on('connection', function (socket) {
     exec("git clone "+data.repoAddr+" sessions/"+data.sessionId, puts);
   });
 
+  socket.on('replaceFile', function(data){
+    console.log(data);
+    var filePath = "sessions/"+data.sessionId+"/"+data.path;
+    fs.writeFile(filePath, data.fileString, function(err) {
+      if(err) { console.log(err); }
+      else { console.log("The file was saved!"); }
+    });
+  });
+
 });
+
+

4fe01fa195001197a535e76fa29d1d49748919ef
clydeli <clyde1008li@gmail.com>
2013-07-22 13:18:05 -0700

    update server code

--- app-client.js
+++ app-client.js
@@ -2,7 +2,7 @@ var liveSocket = io.connect("http://ec2-54-245-222-60.us-west-2.compute.amazonaw
 liveSocket.on('livePreview', function(data){
   console.log(data);
   // example one...
-  liveSocket.emit("clone", {sessionId: "testid", repoAddr: "git@github.com:davliu/sticky.git"} );
+  //liveSocket.emit("clone", {sessionId: "testid", repoAddr: "git@github.com:davliu/sticky.git"} );
   // example two...
-  liveSocket.emit("replaceFile", {sessionId: "testid", fileString: "teststring", path: 'index.html'} );
+  liveSocket.emit("replaceFile", {sessionId: "testid2", fileString: "teststring", path: 'index.html'} );
 });

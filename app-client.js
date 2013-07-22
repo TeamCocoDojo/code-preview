75c011d3b6b51e0206e61273754d26e6c2f3d18a
clydeli <clyde1008li@gmail.com>
2013-07-22 13:28:05 -0700

    update nodejs server code

--- app-client.js
+++
@@ -1,8 +0,0 @@
-var liveSocket = io.connect("http://ec2-54-245-222-60.us-west-2.compute.amazonaws.com", {port: 3333} );
-liveSocket.on('livePreview', function(data){
-  console.log(data);
-  // example one...
-  //liveSocket.emit("clone", {sessionId: "testid", repoAddr: "git@github.com:davliu/sticky.git"} );
-  // example two...
-  liveSocket.emit("replaceFile", {sessionId: "testid2", fileString: "teststring", path: 'index.html'} );
-});
--- app-server.js
+++ app-server.js
@@ -20,18 +20,40 @@ io.sockets.on('connection', function (socket) {
 
   socket.emit('livePreview', {});
 
+  // Clone the repository to the session directory
   socket.on('clone', function(data){
     console.log(data);
     exec("git clone "+data.repoAddr+" sessions/"+data.sessionId, puts);
   });
 
+  // Make the session directory
+  socket.on('makedir', function(data){
+    exec("mkdir sessions/"+data.sessionId, puts);
+  });
+
+  // Replace repository files with the modified ones
   socket.on('replaceFile', function(data){
     console.log(data);
     var filePath = "sessions/"+data.sessionId+"/"+data.path;
     fs.writeFile(filePath, data.fileString, function(err) {
       if(err) { console.log(err); }
-      else { console.log("The file was saved!"); }
+      else {
+        console.log("The file was saved!");
+        socket.emit('fileSaved', {});
+      }
+    });
+  });
+
+  // Compile and run C/C++ code using g++
+  socket.on('compileAndRun', function(data){
+    console.log(data);
+    var
+      fileDir = "sessions/"+data.sessionId,
+      files = data.files.join(" ");
+    exec(fileDir+"/g++ "+files, function(){
+      // run the executable file
     });
   });
 
 });
+

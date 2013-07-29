22fa1a84999ab519eb5372b1c7e3062fca674756
clydeli <clyde1008li@gmail.com>
2013-07-28 20:03:37 -0700

    fix preview callback mechanism

--- app-server.js
+++ app-server.js
@@ -8,37 +8,48 @@ var
   sys = require('sys'),
   exec = require('child_process').exec;
 
+// Some Configurations...
 server.listen(3333);
-
 app.configure(function(){
   app.use(express.static(__dirname + '/sessions'));
 });
 
+// Function for outputs
 function puts(error, stdout, stderr) { sys.puts(stdout) }
 
+// client table
+var clientTable = {};
+
 io.sockets.on('connection', function (socket) {
+
+  //console.log(socket.id);
+  //clientTable[socket.id] = socket;
 
   socket.emit('livePreview', {});
 
   // Clone the repository to the session directory
   socket.on('clone', function(data){
     console.log(data);
-    exec("git clone "+data.repoAddr+" sessions/"+data.sessionId, puts);
+    exec("git clone "+data.repoAddr+" -b "+data.repoBranch+" sessions/"+data.sessionId, function(){
+      socket.emit('serverDirectoryReady', {});
+    });
   });
 
   // Make the session directory
   socket.on('makedir', function(data){
-    exec("mkdir sessions/"+data.sessionId, puts);
+    exec("mkdir sessions/"+data.sessionId, function(){
+      socket.emit('serverDirectoryReady', {});
+    });
   });
 
   // Replace repository files with the modified ones
   socket.on('replaceFile', function(data){
-    console.log(data);
+    console.log('replaceFile');
     var filePath = "sessions/"+data.sessionId+"/"+data.path;
     fs.writeFile(filePath, data.fileString, function(err) {
       if(err) { console.log(err); }
       else {
-        console.log("The file was saved!");
+        console.log("The file is saved!");
         socket.emit('fileSaved', {});
       }
     });
@@ -46,7 +57,7 @@ io.sockets.on('connection', function (socket) {
 
   // Compile and run C/C++ code using g++
   socket.on('compileAndRun', function(data){
-    console.log(data);
+    console.log('compileAndRun');
     var
       fileDir = "sessions/"+data.sessionId,
       files = data.files.join(" ");
@@ -56,4 +67,3 @@ io.sockets.on('connection', function (socket) {
   });
 
 });
-
